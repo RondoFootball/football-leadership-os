a464f459e69a10f57d9a8fa2420afbf43aacd4df
RondoFootball <jos@rondo.football>
2026-04-02 19:28:34 +0200

    init posthog in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,30 @@
+"use client";
+
 import "./globals.css";
+import { useEffect } from "react";
+import posthog from "posthog-js";
 import { Analytics } from "@vercel/analytics/react";
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  useEffect(() => {
+    if (
+      typeof window !== "undefined" &&
+      process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN &&
+      process.env.NEXT_PUBLIC_POSTHOG_HOST
+    ) {
+      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_PROJECT_TOKEN, {
+        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST,
+        defaults: "2026-01-30",
+        capture_pageview: true,
+        capture_pageleave: true,
+      });
+    }
+  }, []);
+
   return (
     <html lang="en">
       <body>
